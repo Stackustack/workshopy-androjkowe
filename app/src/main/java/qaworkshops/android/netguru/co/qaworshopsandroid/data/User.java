306ac237bdf294c12b9bcdb09917f3e1d9220a07
Joanna Jasnowska <joanna.jasnowska@netguru.co>
2017-03-21 17:17:39 +0100

    Add User model

---
+++ app/src/main/java/qaworkshops/android/netguru/co/qaworshopsandroid/data/User.java
@@ -0,0 +1,61 @@
+package qaworkshops.android.netguru.co.qaworshopsandroid.data;
+
+import io.realm.RealmObject;
+
+public class User extends RealmObject {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private long birthday;
+    private String country;
+    private String gender;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(long birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
