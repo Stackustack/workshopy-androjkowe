60ca40a28871300291c9cded03ad232b2bbba5ea
Joanna Jasnowska <joanna.jasnowska@netguru.co>
2017-04-21 01:35:52 +0200

    Add login activity test

---
+++ app/src/androidTest/java/qaworkshops/android/netguru/co/qaworshopsandroid/feature/main/LoginActivityTest.java
@@ -0,0 +1,86 @@
+package qaworkshops.android.netguru.co.qaworshopsandroid.feature.main;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import qaworkshops.android.netguru.co.qaworshopsandroid.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void loginActivityTest() {
+        ViewInteraction textInputEditText = onView(
+                withId(R.id.email));
+        textInputEditText.perform(scrollTo(), replaceText("john.doe@example.com"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText2 = onView(
+                withId(R.id.password));
+        textInputEditText2.perform(scrollTo(), replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.sign_up_button), withText("Sign in"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.drawer_layout),
+                                        0),
+                                2),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
